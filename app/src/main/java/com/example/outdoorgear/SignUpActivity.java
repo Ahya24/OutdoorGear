c9b5c67fc17923d16da297ad425abfcdc0ac906c
Ahya Taufiq Akbar <aufiqakbar@gmail.com>
2023-12-17 00:49:00 +0700

    Connect with Realtime database

--- app/src/main/java/com/example/outdoorgear/SignUpActivity.java
+++ app/src/main/java/com/example/outdoorgear/SignUpActivity.java
@@ -15,13 +15,61 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private FirebaseAuth auth;
+    private DatabaseReference databaseReference;
+
     private EditText signupEmail, signupPassword;
     private Button signupButton;
     private TextView loginRedirectText;
+
+    public class User {
+        private String userId;
+        private String email;
+        private String password;  // Sesuaikan dengan informasi tambahan yang ingin disimpan
+
+        // Konstruktor kosong diperlukan untuk Firebase
+        public User() {
+        }
+
+        // Konstruktor dengan parameter
+        public User(String userId, String email, String password) {
+            this.userId = userId;
+            this.email = email;
+            this.password = password;
+        }
+
+        // Getter dan setter sesuaikan dengan kebutuhan
+        public String getUserId() {
+            return userId;
+        }
+
+        public void setUserId(String userId) {
+            this.userId = userId;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +77,8 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
 
         auth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
+
         signupEmail = findViewById(R.id.signup_email);
         signupPassword = findViewById(R.id.signup_password);
         signupButton = findViewById(R.id.signup_button);
@@ -37,30 +87,51 @@ public class SignUpActivity extends AppCompatActivity {
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String user = signupEmail.getText().toString().trim();
+                final String user = signupEmail.getText().toString().trim();
                 String pass = signupPassword.getText().toString().trim();
 
-                if (user.isEmpty()){
+                // Inisialisasi variabel user
+                final String userEmail;
+
+                if (user.isEmpty()) {
                     signupEmail.setError("Email cannot be empty");
-                }
-                if (pass.isEmpty()){
-                    signupPassword.setError("Password cannot be empty");
-                } else{
-                    auth.createUserWithEmailAndPassword(user, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                Toast.makeText(SignUpActivity.this, "SignUp Successful", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
-                            } else {
-                                Toast.makeText(SignUpActivity.this, "SignUp Failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
+                    return; // Stop execution if the email is empty
+                } else {
+                    userEmail = user;  // Set nilai user jika tidak kosong
                 }
 
+                if (pass.isEmpty()) {
+                    signupPassword.setError("Password cannot be empty");
+                    return; // Stop execution if the password is empty
+                }
+
+                // Continue with Firebase authentication
+                auth.createUserWithEmailAndPassword(userEmail, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Pendaftaran berhasil
+                            Toast.makeText(SignUpActivity.this, "SignUp Successful", Toast.LENGTH_SHORT).show();
+
+                            // Simpan informasi pengguna ke dalam Realtime Database
+                            FirebaseUser currentUser = auth.getCurrentUser();
+                            String userId = currentUser.getUid();
+
+                            User newUser = new User(userId, userEmail, pass); // Sesuaikan dengan struktur User Anda
+                            databaseReference.child(userId).setValue(newUser);
+
+                            // Lanjutkan ke LoginActivity atau lakukan tindakan lainnya
+                            startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
+                        } else {
+                            // Pendaftaran gagal
+                            Toast.makeText(SignUpActivity.this, "SignUp Failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
             }
         });
+
+
 
         loginRedirectText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,6 +139,5 @@ public class SignUpActivity extends AppCompatActivity {
                 startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
             }
         });
-
     }
 }

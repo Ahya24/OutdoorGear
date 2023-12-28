d313c64a6463417975054041993680f4ac182c41
DeoTriyanuar <deoyanuar20@gmail.com>
2023-12-28 07:08:52 +0700

    checkout layout 1

--- app/src/main/java/com/example/outdoorgear/HomeFragment.java
+++ app/src/main/java/com/example/outdoorgear/HomeFragment.java
@@ -4,15 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
 
 public class HomeFragment extends Fragment {
+
+    private ImageButton hedon;
+    private ImageButton fullpack;
+    private ImageButton ceria;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
+
+
     }
 }
